10fdf4bb5de1465e337c8ba89df868c8eec84bd2
NellieMK65 <61111072+NellieMK65@users.noreply.github.com>
2022-09-02 09:59:30 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide() {
+    let a=2000;
+    let b=100;
+    return a/b
+}
+
+const square = (parameter1) => parameter1**2;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
